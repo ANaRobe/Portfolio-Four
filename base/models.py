21608b62339f75c7c946d2b8f7af82fb74dd10fb
ANaRobe <ana.maria.robe@gmail.com>
2022-09-25 06:41:38 +0000

    build custom models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,72 @@
+"""Database Tables"""
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+STATUS = ((0, 'Draft'), (1, 'Published'))
+
+
+class Cocktail(models.Model):
+    """Cocktail recipe blueprint"""
+    title = models.CharField(max_length=50, null=False, blank=False, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    image = CloudinaryField('image', default='placeholder')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='remarks', null=False, blank=False,)
+    likes = models.ManyToManyField(User, related_name='cocktail_hearts', blank=False)
+    ingredients = models.TextField(null=False, blank=False)
+    steps = models.TextField(null=False, blank=False)
+    mixing_time = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(60)])
+    status = models.IntegerField(choices=STATUS, default=0)
+    publish_date = models.DateTimeField(auto_now=True)
+    last_update = models.DateTimeField(auto_now=True)
+
+    SWEET = 'sweet'
+    BITTER = 'bitter' 
+    SOUR = 'sour'
+    SALTY = 'salty'
+    UMAMI = 'umami'
+    COOL = 'cool'
+    HOT = 'hot'
+
+    COCKTAIL_TASTE = [
+        (SWEET, 'sweet'),
+        (BITTER, 'bitter'),
+        (SOUR, 'sour'),
+        (SALTY, 'salty'),
+        (UMAMI, 'umami'),
+        (COOL, 'cool'),
+        (HOT, 'hot')
+    ]
+
+    taste = models.CharField(max_length=50, choices=COCKTAIL_TASTE, default=COOL, blank=False)
+
+    class Meta:
+        """Orders articles by title """
+        ordering = ['title']
+
+    def __str__(self):
+        return f"{self.title} | {self.user}"
+
+    def number_of_likes(self):
+        """Returns the total number of likes for each published cocktail"""
+        return self.likes.count()
+
+
+class Remark(models.Model):
+    """Layout for comments"""
+    cocktail = models.ForeignKey(Cocktail, on_delete=models.CASCADE, related_name='cocktails', null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='remarks_user')
+    text = models.TextField(null=False, blank=False)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(
+        default=False)
+
+    class Meta:
+        """Orders articles by published date ascendant """
+        ordering = ['publish_date']
+
+    def __str__(self):
+        return f"Comment{self.text} by {self.user}"
+
