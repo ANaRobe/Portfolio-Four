56f74f24c34ba9b7dbf7b9f4c3a9ac2f4751aeeb
ANaRobe <ana.maria.robe@gmail.com>
2022-10-07 11:50:35 +0000

    fix urls

--- base/urls.py
+++ base/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', CocktailsList.as_view(), name='home'),
-    path('<slug:slug>/', CocktailDetail.as_view(), name='cocktail'),
     path('add/', CocktailCreate.as_view(), name='add'),
     path('edit/<slug:slug>/', CocktailEdit.as_view(), name='edit'),
+    path('<slug:slug>/', CocktailDetail.as_view(), name='cocktail'),
 ]
