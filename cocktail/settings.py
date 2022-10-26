f9c66616145c784b3999a5e88d2956f270f50b33
ANaRobe <ana.maria.robe@gmail.com>
2022-10-26 09:10:37 +0000

    correct syntax sameorigin

--- cocktail/settings.py
+++ cocktail/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-X-FRSME_OPTIONS ="SAMEORIGIN"
+X-FRSME_OPTIONS == "SAMEORIGIN"
 ALLOWED_HOSTS = ['cocktail2022.herokuapp.com', 'localhost']
 
 
