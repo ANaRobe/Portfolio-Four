ce093a9bee5a29a520b393ea443bbac2019c868a
ANaRobe <ana.maria.robe@gmail.com>
2022-10-26 09:42:34 +0000

    X_FRAME_OPTIONS correct

--- cocktail/settings.py
+++ cocktail/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-X-FRAME_OPTIONS == "SAMEORIGIN"
+X_FRAME_OPTIONS == "SAMEORIGIN"
 ALLOWED_HOSTS = ['cocktail2022.herokuapp.com', 'localhost']
 
 
